cafe7fb6526fd4e754c82caae324d145abd56d56
Julien <julien.xemard@outlook.com>
2021-02-12 10:02:29 +0100

    Html Init

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../style/style.scss'
+
+const App = () => {
+
+  return (
+
+    <>
+      <h1>Welcome to Hub'Sales</h1>
+    </>
+
+  )
+
+}
+
+export default App
